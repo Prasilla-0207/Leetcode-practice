d5ce583bdf7c100bbde2d60fc4988dd6b8d7d867
Prasilla <prasillaprassanna@gmail.com>
2026-08-11 12:31:19 +0530

    Initial commit

---
+++ twosum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map= new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],i);
+        }
+        for(int i=0;i<nums.length;i++){
+            int tmp=target-nums[i];
+            if(map.containsKey(tmp)&&map.get(tmp)!=i){
+                return new int[]{i,map.get(tmp)};
+            }
+        }
+        System.out.print(map);
+        return new int[] {};
+    }
+}
